8bd8c8364451dd340ff5ad52d36f47afbdc63c86
Kalle M. Aagaard <km@conscia.com>
2024-06-05 09:45:18 +0000

    Release

--- src/keyring_proxy_socket/__init__.py
+++ src/keyring_proxy_socket/__init__.py
@@ -1,7 +1,7 @@
 from typing import override
 
 from jaraco.classes import properties
-from keyring_proxy.socketproxy import DEFAULT_SOCKET_PATH, SocketClient
+from keyring_proxy.socketproxy import SocketClient, default_socket_mgr
 from keyring_proxy.transport import ProxyBackend, TransportClient
 
 PRIORITY = 9.9
@@ -9,11 +9,13 @@ PRIORITY = 9.9
 
 class SocketProxyBackend(ProxyBackend):
 
-    socket: str = DEFAULT_SOCKET_PATH
+    socket_path: str | None = None
+    socket_ip: str | None = None
+    socket_port: str | int | None = None
 
     @override
     def _get_transport(self) -> TransportClient:
-        return SocketClient.from_path(self.socket)
+        return SocketClient(default_socket_mgr(self.socket_path, self.socket_ip, self.socket_port))
 
     @properties.classproperty
     def priority(cls):
